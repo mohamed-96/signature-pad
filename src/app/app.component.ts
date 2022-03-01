ab698b8afce1137abec5970ac9c72747666e65c9
mmediene <mohamedlamine.mediene@smartest.dz>
2022-03-01 21:08:38 +0100

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,48 @@
+import { Component, ElementRef, ViewChild, ViewEncapsulation } from '@angular/core';
+import SignaturePad from 'signature_pad';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+
+})
+export class AppComponent {
+  title = 'signatureJS';
+  signaturePad: SignaturePad;
+  @ViewChild('canvas') canvasEl: ElementRef;
+
+  constructor() { }
+
+  ngAfterViewInit() {
+    this.signaturePad = new SignaturePad(this.canvasEl.nativeElement);
+  }
+
+  startDrawing(event: Event) {
+    console.log(event);
+    // works in device not in browser
+
+  }
+
+  moved(event: Event) {
+    // works in device not in browser
+  }
+
+  clearPad() {
+    this.signaturePad.clear();
+  }
+
+  savePad() {
+    const base64Data = this.signaturePad.toDataURL();
+
+    var a = document.createElement("a"); //Create <a>
+    a.href = base64Data; //Image Base64 Goes here
+    a.download = new Date().getTime()+"-Image.png"; //File name Here
+    a.click(); //Downloaded file
+  }
+
+
+
+
+
+}
